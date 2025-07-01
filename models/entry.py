934a59b6a43542abdc224928ef1d5cf79d58c77c
nijeDakic <pavledakicpaja@gmail.com>
2025-07-01 05:20:34 +0200

    clean code in entry class

--- models/entry.py
+++ models/entry.py
@@ -2,18 +2,11 @@ from utils import extract_id
 import datetime
 
 class Entry:
-    id: int
-    title: str
-    link: str
-    author: str
-    date: datetime
-    summary: str
-
     def __init__(self, rss_entry):
-        self.id = extract_id(rss_entry.id)
-        self.title = rss_entry.title
-        self.link = rss_entry.link
-        self.author = rss_entry.author
-        self.date = datetime.fromisoformat(rss_entry.published)
-        self.summary = rss_entry.summary
+        self.id: int = extract_id(rss_entry.id)
+        self.title: str = rss_entry.title
+        self.link: str = rss_entry.link
+        self.author: str = rss_entry.author
+        self.date: datetime = datetime.fromisoformat(rss_entry.published)
+        self.summary: str = rss_entry.summary
     
